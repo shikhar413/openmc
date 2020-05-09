9efe4c31db35ddcd14d863d88dc9bdcc56fbb3ad
Shikhar Kumar <shikhark@mit.edu>
2020-05-08 22:54:19 -0400

    Clean up scripts for CMFD thread sensitivity, need to merge with param_sensitivity

---
+++ examples/phd_testing/cmfd-testing/thread_sensitivity/base/run_ea_cmfd.py
@@ -0,0 +1,161 @@
+import openmc.lib
+from openmc.ensemble_averaging import EnsAvgCMFDRun
+from openmc import cmfd
+import openmc.lib as capi
+import glob
+import os
+import sys
+import numpy as np
+from mpi4py import MPI
+
+def init_prob_params(problem_type, mesh_type):
+    cmfd_mesh = cmfd.CMFDMesh()
+    if problem_type == '1d-homog':
+        cmfd_mesh.lower_left = [-5., -5., -200.]
+        cmfd_mesh.upper_right = [5., 5., 200.]
+        cmfd_mesh.albedo = [1., 1., 1., 1., 0., 0.]
+
+        invalid_mesh = not mesh_type.endswith("cm")
+        if not invalid_mesh:
+            try:
+                mesh_sz = float(mesh_type[:-2])
+            except:
+                invalid_mesh = True
+        if invalid_mesh:
+            err_msg = 'Logic for 1D mesh type {} has not been defined yet'
+            print(err_msg.format(mesh_type))
+            sys.exit()
+        cmfd_mesh.dimenion = [1, 1, int(400./mesh_sz)]
+
+    elif problem_type == '2d-beavrs':
+        cmfd_mesh.lower_left = [-182.78094, -182.78094, 220.0]
+        cmfd_mesh.upper_right = [182.78094, 182.78094, 240.0]
+        cmfd_mesh.albedo = [0., 0., 0., 0., 1., 1.]
+        cmfd_mesh.energy = [0.0, 0.625, 20000000]
+
+        mesh_dim, mesh_map = get_2db_mesh_properties(mesh_type)
+        cmfd_mesh.dimension = mesh_dim
+        cmfd_mesh.map = mesh_map
+
+    else:
+        err_msg = 'Logic for problem type {} has not been defined yet'
+        print(err_msg.format(problem_type))
+        sys.exit()
+
+    return cmfd_mesh
+
+def get_2db_mesh_properties(mesh_type):
+    n_assembly_x = 17
+    n_assembly_y = 17
+    mesh_properties = {
+        'assembly': {
+            'mesh_refinement_x': 1,
+            'mesh_refinement_y': 1,
+            'assembly_map_layout': [1]
+        },
+        'qassembly': {
+            'mesh_refinement_x': 2,
+            'mesh_refinement_y': 2,
+            'assembly_map_layout': [1, 1, 1, 1]
+        },
+        'pincell': {
+            'mesh_refinement_x': 17,
+            'mesh_refinement_y': 17,
+            'assembly_map_layout': [
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1,
+1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+            ]
+        },
+    }
+    assembly_map = np.array([
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]).reshape(n_assembly_x, n_assembly_y)
+    if mesh_type not in mesh_properties:
+        err_msg = 'Logic for 2D mesh type {} has not been defined yet'
+        print(err_msg.format(mesh_type))
+        sys.exit()
+
+    mesh_refinement_x = mesh_properties[mesh_type]['mesh_refinement_x']
+    mesh_refinement_y = mesh_properties[mesh_type]['mesh_refinement_y']
+    assembly_mesh_map = np.array(mesh_properties[mesh_type]['assembly_map_layout']).reshape(mesh_refinement_x, mesh_refinement_y)
+    mesh_map = np.zeros((n_assembly_x*mesh_refinement_x, 
+                         n_assembly_y*mesh_refinement_y),
+                        dtype=int)
+    mesh_dim = mesh_map.shape + (1,)
+
+    for i in range(n_assembly_x):
+        for j in range(n_assembly_y):
+            if assembly_map[i, j]:
+                mesh_map[i*mesh_refinement_x:(i+1)*mesh_refinement_y, j*mesh_refinement_x:(j+1)*mesh_refinement_y] = assembly_mesh_map
+
+    return mesh_dim, mesh_map.flatten()
+
+if __name__ == "__main__":
+
+    # IGNORE STATEPOINTS FOR NOW
+    '''
+    # Find restart file
+    statepoints = glob.glob(os.path.join("statepoint.*.h5"))
+
+    # Restart file exists
+    if len(statepoints) == 1:
+        res_file = statepoints[0]
+        args=["-s",n_threads,"-r",res_file]
+
+        # Load all simulation variables from disk
+        prev_sp = res_file
+        fet_data = np.load('fet_data.npy')
+        entropy_data = np.load('entropy_data.npy')
+
+    # Restart file doesn't exist
+    elif len(statepoints) == 0:
+        args=['-s', n_threads]
+
+    else:
+        print("Multiple statepoint files found. Please retry")
+        sys.exit()
+    '''
+    # Get problem type as command line argument
+    prob_type = sys.argv[1]
+    mesh_type = sys.argv[2]
+    cmfd_mesh = init_prob_params(prob_type, mesh_type)
+    statepoint_interval = 10
+
+    # Initialize and run CMFDRun object
+    ea_cmfd_run = EnsAvgCMFDRun()
+    ea_cmfd_run.cfg_file = "params.cfg"
+    ea_cmfd_run.mesh = cmfd_mesh
+    with ea_cmfd_run.run_in_memory():
+        for _ in ea_cmfd_run.iter_batches():
+            pass
---
+++ examples/phd_testing/cmfd-testing/thread_sensitivity/scripts/cmfd_thread_sensitivity.py
@@ -0,0 +1,123 @@
+import sys
+import socket
+import os
+
+def generate_input_files(cluster, run_file):
+    if cluster == "Green Cluster":
+        cluster_params = {
+            'prob_type': "2d-beavrs",
+            'particles': 10000000,
+            'mesh_types': ['assembly', 'qassembly', 'pincell'],
+            'batch_file': "job.slurm",
+            'run_command': "sbatch ",
+            'xml_files': ['2db-settings.xml', '2db-geometry.xml', '2db-materials.xml'],
+            'ppn': 32,
+            'nodes': [2],
+            'walltime': '12:00:00',
+            'cmfd_threads': [1,2,4,8,16],
+            'ref_d': '[1.42669, 0.400433]',
+            'n_batches': 10,
+            'n_inactive': 9
+        }
+
+    os.system('mkdir -p ./../cmfd-thread-sensitivity')
+    os.chdir('./../cmfd-thread-sensitivity')
+
+    # Generate files for base run
+    with open('../base/params.cfg', 'r') as file:
+        param_template = file.read()
+    with open('../base/{}'.format(cluster_params['batch_file']), 'r') as file:
+        batch_template = file.read()
+
+    os.system('mkdir -p {}'.format(cluster_params['prob_type']))
+    os.chdir(cluster_params['prob_type'])
+
+    # Create files for cmfd case
+    for mesh in cluster_params['mesh_types']:
+        mesh_dir = 'cmfd-{}-mesh'.format(mesh)
+        os.system('mkdir -p {}'.format(mesh_dir))
+        os.chdir(mesh_dir)
+        nodes = cluster_params['nodes']
+        # TODO add 11 nodes explicitly for pincell CMFD mesh
+        for node in nodes:
+            for thread in cluster_params['cmfd_threads']:
+                nodethread_dir = '{}node{}thread'.format(node-1, thread)
+                os.system('mkdir -p {}'.format(nodethread_dir))
+                os.chdir(nodethread_dir)
+                for xml_file in cluster_params['xml_files']:
+                    os.system('cp ./../../../../base/{} ./{}'.format(xml_file, xml_file.split('-')[-1]))
+                os.system('cp ./../../../../base/run_ea_cmfd.py ./run_openmc.py')
+                create_files(param_template, batch_template, cluster_params, node, thread, run_file, mesh)
+                os.chdir('./..')
+        os.chdir('./..')
+    os.chdir('./..')
+
+    # TODO create test cases for 1d-homog case (0.4cm mesh vs. 20cm mesh)
+    # TODO add cases for when n_threads is fixed and add entropy, fet calculation
+
+
+def create_files(param_template, batch_template, cluster_params, node, thread, run_file, mesh):
+    os.system('sed -i s@\</fet_convergence@\</fet_convergence--@g settings.xml')
+    os.system('sed -i s@\<fet@\<\!--fet@g settings.xml')
+    os.system('sed -i s@\<mesh@\<\!--mesh@g settings.xml')
+    os.system('sed -i s@\</entropy_mesh@\</entropy_mesh--@g settings.xml')
+    jobname = 'nth{}{}'.format(thread, mesh)
+    nodes = node
+    tasks = nodes * cluster_params['ppn']
+    nprocs = nodes*2
+    nthreads = int(cluster_params['ppn']/2)
+
+    batch_template = batch_template.replace('{prob_type}', mesh)
+    batch_template = batch_template.replace('{tasks}', str(tasks))
+    batch_template = batch_template.replace('{nodes}', str(nodes))
+    batch_template = batch_template.replace('{ppn}', str(cluster_params['ppn']))
+    batch_template = batch_template.replace('{job_name}', jobname)
+    batch_template = batch_template.replace('{walltime}', cluster_params['walltime'])
+    batch_template = batch_template.replace('{nprocs}', str(nprocs))
+    batch_template = batch_template.replace('{nthreads}', cluster_params['prob_type'])
+    with open(cluster_params['batch_file'], 'w') as f:
+        f.write(batch_template)
+
+    param_template = param_template.replace('{ref_d}', cluster_params['ref_d'])
+    param_template = param_template.replace('{n_threads}', str(thread))
+    # TODO set batches and inactive differently for pincell mesh
+    param_template = param_template.replace('{n_batches}', str(cluster_params['n_batches']))
+    param_template = param_template.replace('{n_inactive}', str(cluster_params['n_inactive']))
+
+    with open('params.cfg', 'w') as f:
+        f.write(param_template)
+
+    print_str = os.getcwd().split('cmfd-thread-sensitivity/')[-1]
+    if run_file:
+        print('Running batch script in {}'.format(print_str))
+        os.system(cluster_params['run_command']+cluster_params['batch_file'])
+    else:
+        print('Created input files in {}'.format(print_str))
+
+
+def get_cluster(socket_name):
+    if socket_name.startswith('nsecluster'):
+        return "NSE Cluster"
+    elif socket_name.startswith('eofe'):
+        return "Green Cluster"
+    elif socket_name.startswith('sk-linux'):
+        return "SK Linux"
+    elif socket_name.startswith('falcon'):
+        return "INL Cluster"
+    else:
+        print('Unrecognized socket name')
+        sys.exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) not in [1, 2]:
+        print('Usage: tally_solver_begin_sensitivity.py [-r]')
+        sys.exit()
+
+    # Get command line arguments
+    run_file = len(sys.argv) == 2 and sys.argv[1] == '-r'
+
+    # Get cluster where script is running on
+    cluster = get_cluster(socket.gethostname())
+
+    # Generate OpenMC and batch script input files
+    generate_input_files(cluster, run_file)
