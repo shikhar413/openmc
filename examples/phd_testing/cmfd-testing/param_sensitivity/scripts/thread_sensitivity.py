ea939babcdd3e15db5421df2a842a6658d414e95
Shikhar Kumar <shikhark@mit.edu>
2020-05-11 23:07:34 -0400

    Generate all thread sensitivty cases and merge with param_sensitivity directory

--- examples/phd_testing/cmfd-testing/param_sensitivity/scripts/thread_sensitivity.py
+++ examples/phd_testing/cmfd-testing/param_sensitivity/scripts/thread_sensitivity.py
@@ -7,7 +7,7 @@ def generate_input_files(cluster, run_file):
         cluster_params = {
             'prob_type': "2d-beavrs",
             'particles': 10000000,
-            'mesh_types': ['assembly', 'qassembly', 'pincell'],
+            'mesh_types': ['assembly', 'qassembly', 'pincell', 'none'],
             'batch_file': "job.slurm",
             'run_command': "sbatch ",
             'xml_files': ['2db-settings.xml', '2db-geometry.xml', '2db-materials.xml'],
@@ -16,12 +16,12 @@ def generate_input_files(cluster, run_file):
             'walltime': '12:00:00',
             'cmfd_threads': [1,2,4,8,16],
             'ref_d': '[1.42669, 0.400433]',
-            'n_batches': 10,
-            'n_inactive': 9
+            'n_batches': 100,
+            'n_inactive': 99
         }
 
-    os.system('mkdir -p ./../cmfd-thread-sensitivity')
-    os.chdir('./../cmfd-thread-sensitivity')
+    os.system('mkdir -p ./../thread-sensitivity')
+    os.chdir('./../thread-sensitivity')
 
     # Generate files for base run
     with open('../base/params.cfg', 'r') as file:
@@ -34,21 +34,33 @@ def generate_input_files(cluster, run_file):
 
     # Create files for cmfd case
     for mesh in cluster_params['mesh_types']:
-        mesh_dir = 'cmfd-{}-mesh'.format(mesh)
+        if mesh == 'none':
+            mesh_dir = 'nocmfd'
+        else:
+            mesh_dir = 'cmfd-{}-mesh'.format(mesh)
         os.system('mkdir -p {}'.format(mesh_dir))
         os.chdir(mesh_dir)
         nodes = cluster_params['nodes']
-        # TODO add 11 nodes explicitly for pincell CMFD mesh
-        for node in nodes:
-            for thread in cluster_params['cmfd_threads']:
-                nodethread_dir = '{}node{}thread'.format(node-1, thread)
-                os.system('mkdir -p {}'.format(nodethread_dir))
-                os.chdir(nodethread_dir)
-                for xml_file in cluster_params['xml_files']:
-                    os.system('cp ./../../../../base/{} ./{}'.format(xml_file, xml_file.split('-')[-1]))
-                os.system('cp ./../../../../base/run_ea_cmfd.py ./run_openmc.py')
-                create_files(param_template, batch_template, cluster_params, node, thread, run_file, mesh)
-                os.chdir('./..')
+        if mesh == 'pincell':
+            nodes = [2, 11]
+        if mesh == 'none':
+            for xml_file in cluster_params['xml_files']:
+                os.system('cp ./../../../base/{} ./{}'.format(xml_file, xml_file.split('-')[-1]))
+            thread = None
+            node = 1
+            create_files(param_template, batch_template, cluster_params, node, thread, run_file, mesh)
+            os.system('sed -i "s/python3 .*/openmc -s 16/g" job.slurm')
+        else:
+            for node in nodes:
+                for thread in cluster_params['cmfd_threads']:
+                    nodethread_dir = '{}node{}thread'.format(node-1, thread)
+                    os.system('mkdir -p {}'.format(nodethread_dir))
+                    os.chdir(nodethread_dir)
+                    for xml_file in cluster_params['xml_files']:
+                        os.system('cp ./../../../../base/{} ./{}'.format(xml_file, xml_file.split('-')[-1]))
+                    os.system('cp ./../../../../base/run_ea_cmfd.py ./run_openmc.py')
+                    create_files(param_template, batch_template, cluster_params, node, thread, run_file, mesh)
+                    os.chdir('./..')
         os.chdir('./..')
     os.chdir('./..')
 
@@ -61,7 +73,10 @@ def create_files(param_template, batch_template, cluster_params, node, thread, r
     os.system('sed -i s@\<fet@\<\!--fet@g settings.xml')
     os.system('sed -i s@\<mesh@\<\!--mesh@g settings.xml')
     os.system('sed -i s@\</entropy_mesh@\</entropy_mesh--@g settings.xml')
-    jobname = 'nth{}{}'.format(thread, mesh)
+    if mesh == 'none':
+        jobname = 'nocmfd'
+    else:
+        jobname = 'nth{}{}'.format(thread, mesh)
     nodes = node
     tasks = nodes * cluster_params['ppn']
     nprocs = nodes*2
@@ -78,16 +93,21 @@ def create_files(param_template, batch_template, cluster_params, node, thread, r
     with open(cluster_params['batch_file'], 'w') as f:
         f.write(batch_template)
 
-    param_template = param_template.replace('{ref_d}', cluster_params['ref_d'])
-    param_template = param_template.replace('{n_threads}', str(thread))
-    # TODO set batches and inactive differently for pincell mesh
-    param_template = param_template.replace('{n_batches}', str(cluster_params['n_batches']))
-    param_template = param_template.replace('{n_inactive}', str(cluster_params['n_inactive']))
+    if mesh != 'none':
+        param_template = param_template.replace('{ref_d}', cluster_params['ref_d'])
+        param_template = param_template.replace('{n_threads}', str(thread))
+        n_batches = str(cluster_params['n_batches'])
+        n_inactive = str(cluster_params['n_inactive'])
+        if mesh == 'pincell':
+            n_batches = '10'
+            n_inactive = '9'
+        param_template = param_template.replace('{n_batches}', n_batches)
+        param_template = param_template.replace('{n_inactive}', n_inactive)
 
-    with open('params.cfg', 'w') as f:
-        f.write(param_template)
+        with open('params.cfg', 'w') as f:
+            f.write(param_template)
 
-    print_str = os.getcwd().split('cmfd-thread-sensitivity/')[-1]
+    print_str = os.getcwd().split('thread-sensitivity/')[-1]
     if run_file:
         print('Running batch script in {}'.format(print_str))
         os.system(cluster_params['run_command']+cluster_params['batch_file'])
