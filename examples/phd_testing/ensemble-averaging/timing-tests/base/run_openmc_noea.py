7c01190104a96f6919a4db1cbbf9d8125bea0515
Shikhar Kumar <shikhark@mit.edu>
2020-08-18 21:40:16 -0400

    Add scipt to generate example scripts for comparing EA, CMFD, and noCMFD timing on LCRC

---
+++ examples/phd_testing/ensemble-averaging/timing-tests/base/run_openmc_noea.py
@@ -0,0 +1,361 @@
+from openmc import cmfd
+import glob
+import os
+import sys
+from mpi4py import MPI
+import openmc.lib as capi
+import numpy as np
+
+def init_openmc_run(problem_type, test_num, max_window_size):
+    test_num_dict = {
+        '0': {
+            'runtype': 'nocmfd',
+            'cmfd-attrs': {
+                'window_type': 'expanding'
+            }
+        },
+        '1': {
+            'runtype': 'cmfd',
+            'cmfd-attrs': {
+                'window_type': 'expanding'
+            },
+            'solver_end': True
+        },
+        '2': {
+            'runtype': 'cmfd',
+            'cmfd-attrs': {
+                'window_type': 'expanding',
+            }
+        }
+    }
+
+    solver_end_dict = {
+        '1d-homog': 150,
+        '1d-homog-offset': 150,
+        '2d-beavrs': 150,
+    }
+
+    if test_num not in test_num_dict:
+        print('Logic for test number {} not defined')
+        sys.exit()
+
+    params = test_num_dict[test_num]
+
+    if 'solver_end' in params:
+        params['cmfd-attrs']['solver_end'] = solver_end_dict[problem_type]
+
+    if test_num in ['1', '2']:
+        if max_window_size is not None:
+            params['cmfd-attrs']['max_window_size'] = max_window_size
+        if '1d-homog' in problem_type:
+            mesh_type = '20cm'
+        else:
+            mesh_type = 'qassembly'
+    else:
+        mesh_type = 'none'
+
+    # Define CMFD parameters
+    cmfd_mesh = cmfd.CMFDMesh()
+    if '1d-homog' in problem_type:
+        cmfd_mesh.lower_left = [-5., -5., -200.]
+        cmfd_mesh.upper_right = [5., 5., 200.]
+        cmfd_mesh.albedo = [1., 1., 1., 1., 0., 0.]
+        mesh_dim, mesh_map = get_mesh_properties(mesh_type)
+        cmfd_mesh.dimension = mesh_dim
+
+    elif problem_type == '2d-beavrs':
+        cmfd_mesh.lower_left = [-182.78094, -182.78094, 220.0]
+        cmfd_mesh.upper_right = [182.78094, 182.78094, 240.0]
+        cmfd_mesh.albedo = [0., 0., 0., 0., 1., 1.]
+        cmfd_mesh.energy = [0.0, 0.625, 20000000]
+
+        mesh_dim, mesh_map = get_mesh_properties(mesh_type, is_1d=False)
+        cmfd_mesh.dimension = mesh_dim
+        cmfd_mesh.map = mesh_map
+    else:
+        err_msg = 'Logic for problem type {} has not been defined yet'
+        print(err_msg.format(problem_type))
+        sys.exit()
+
+    # Initialize CMFDRun object
+    cmfd_run = cmfd.CMFDRun()
+
+    # Set all runtime parameters (cmfd_mesh, tolerances, tally_resets, etc)
+    # All error checking done under the hood when setter function called
+    cmfd_run.mesh = cmfd_mesh
+    if problem_type == '1d-homog':
+        cmfd_run.ref_d = []
+        if params['runtype'] == 'nocmfd':
+            cmfd_run.tally_begin = 2000
+            cmfd_run.solver_begin = 2000
+        else:
+            cmfd_run.tally_begin = 1
+            cmfd_run.solver_begin = 1
+    elif problem_type == '1d-homog-offset':
+        cmfd_run.ref_d = []
+        if params['runtype'] == 'nocmfd':
+            cmfd_run.tally_begin = 2000
+            cmfd_run.solver_begin = 2000
+        else:
+            cmfd_run.tally_begin = 10
+            cmfd_run.solver_begin = 20
+    else:
+        cmfd_run.ref_d = [1.42669, 0.400433]
+        if params['runtype'] == 'nocmfd':
+            cmfd_run.tally_begin = 2000
+            cmfd_run.solver_begin = 2000
+        else:
+            cmfd_run.tally_begin = 2
+            cmfd_run.solver_begin = 3
+
+    cmfd_run.display = {'balance': True, 'dominance': True, 'entropy': False, 'source': True}
+    cmfd_run.feedback = True
+    cmfd_run.downscatter = True
+    cmfd_run.gauss_seidel_tolerance = [1.e-15, 1.e-20]
+    if mesh_type in ['pincell', '0p4cm']:
+        cmfd_run.use_all_threads = True
+
+    for attr in params['cmfd-attrs']:
+        setattr(cmfd_run, attr, params['cmfd-attrs'][attr])
+
+    return cmfd_run
+
+def init_fet_params(problem_type):
+    if '1d-homog' in problem_type:
+        n_modes = 10
+        labels = np.array(['P{}'.format(str(i)) for i in range(n_modes+1)])
+        coeffs = np.zeros((len(labels), 1),dtype=float)
+        for j in range(len(labels)):
+            coeffs[j,0] = (2*int(labels[j].split("P")[-1])+1)/2
+
+    elif problem_type == '2d-beavrs':
+        n_radial_modes = 10
+        n_coeffs = int((n_radial_modes+1)*(n_radial_modes+2)/2)
+        labels = np.array([])
+        coeffs = np.zeros((n_coeffs, 1),dtype=float)
+        j = 0
+        for n in range(n_radial_modes+1):
+            for m in range(-1*n, n+2, 2):
+                labels = np.append(labels, 'Z{},{}'.format(str(n), str(m))) 
+                if m == 0:
+                    coeffs[j,0] = n+1
+                else:
+                    coeffs[j,0] = 2*n + 2
+                j += 1
+
+    elif problem_type == '3d-exasmr':
+        n_coeffs = int((n_radial+1)*(n_radial+2)/2)*(n_axial+1)
+        labels = np.array([])
+        coeffs = np.zeros((n_coeffs, 2),dtype=float)
+        j = 0
+        for n in range(n_radial+1):
+            for m in range(-1*n, n+2, 2):
+                for l in range(n_axial+1):
+                    labels = np.append(labels, 'Z{},{}-P{}'.format(str(n), str(m), str(l)))
+                    if m == 0:
+                        coeffs[j,0] = (n+1)
+                        coeffs[j,1] = (2*l+1)/2
+                    else:
+                        coeffs[j,0] = (2*n+2)
+                        coeffs[j,1] = (2*l+1)/2
+                    j += 1
+
+    else:
+        err_msg = 'Logic for problem type {} has not been defined yet'
+        print(err_msg.format(problem_type))
+        sys.exit()
+
+    return labels, coeffs
+
+def get_mesh_properties(mesh_type, is_1d=True):
+    if is_1d:
+        mesh_dims = {
+            'none': [1, 1, 1],
+            '4cm': [1, 1, 100],
+            '1cm': [1, 1, 400],
+            '20cm': [1, 1, 20],
+            '0p4cm': [1, 1, 1000],
+        }
+        if mesh_type not in mesh_dims:
+            err_msg = 'Logic for 1D mesh type {} has not been defined yet'
+            print(err_msg.format(mesh_type))
+            sys.exit()
+        return mesh_dims[mesh_type], None
+            
+    else:
+        n_assembly_x = 17
+        n_assembly_y = 17
+        mesh_properties = {
+            'none': {
+                'mesh_refinement_x': 1,
+                'mesh_refinement_y': 1,
+                'assembly_map_layout': [1]
+            },
+            'assembly': {
+                'mesh_refinement_x': 1,
+                'mesh_refinement_y': 1,
+                'assembly_map_layout': [1]
+            },
+            'qassembly': {
+                'mesh_refinement_x': 2,
+                'mesh_refinement_y': 2,
+                'assembly_map_layout': [1, 1, 1, 1]
+            },
+            'pincell': {
+                'mesh_refinement_x': 17,
+                'mesh_refinement_y': 17,
+                'assembly_map_layout': [
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1,
+1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                ]
+            },
+        }
+        assembly_map = np.array([
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]).reshape(n_assembly_x, n_assembly_y)
+        if mesh_type not in mesh_properties:
+            err_msg = 'Logic for 2D mesh type {} has not been defined yet'
+            print(err_msg.format(mesh_type))
+            sys.exit()
+
+        mesh_refinement_x = mesh_properties[mesh_type]['mesh_refinement_x']
+        mesh_refinement_y = mesh_properties[mesh_type]['mesh_refinement_y']
+        assembly_mesh_map = np.array(mesh_properties[mesh_type]['assembly_map_layout']).reshape(mesh_refinement_x, mesh_refinement_y)
+        mesh_map = np.zeros((n_assembly_x*mesh_refinement_x, 
+                             n_assembly_y*mesh_refinement_y),
+                            dtype=int)
+        mesh_dim = mesh_map.shape + (1,)
+
+        for i in range(n_assembly_x):
+            for j in range(n_assembly_y):
+                if assembly_map[i, j]:
+                    mesh_map[i*mesh_refinement_x:(i+1)*mesh_refinement_y, j*mesh_refinement_x:(j+1)*mesh_refinement_y] = assembly_mesh_map
+
+        return mesh_dim, mesh_map.flatten()
+
+if __name__ == "__main__":
+    # Define global MPI communicator
+    global_comm = MPI.COMM_WORLD
+
+    # Get number of seeds and procs per seed  as command line args
+    n_seeds = int(sys.argv[1])
+
+    # Define local MPI communicator
+    available_procs = global_comm.Get_size()
+    n_procs_per_seed = int(available_procs/n_seeds)
+    color = int(global_comm.Get_rank()/n_procs_per_seed)
+    seed = color + 1
+    local_comm =  MPI.Comm.Split(global_comm, color=color)
+
+    # Get number of OpenMP threads as command line arg
+    n_threads = sys.argv[2]
+
+    # Get problem type as command line argument
+    prob_type = sys.argv[3]
+
+    # Get test number as command line argument
+    test_num = sys.argv[4]
+
+    # Get max window size as command line argument if provided
+    max_window_size = int(sys.argv[5]) if len(sys.argv) == 6 else None
+
+    labels, coeffs = init_fet_params(prob_type)
+
+    '''
+    # Find restart file
+    statepoints = glob.glob(os.path.join("statepoint.*.h5")) 
+
+    # Restart file exists
+    if len(statepoints) == 1:
+        res_file = statepoints[0]
+        args=["-s",n_threads,"-r",res_file]
+
+        # Load all simulation variables from disk
+        prev_sp = res_file
+        fet_data = np.load('fet_data.npy')
+        #entropy_data = np.load('entropy_data.npy')
+
+    # Restart file doesn't exist
+    elif len(statepoints) == 0:
+        args=['-s', n_threads]
+
+    else:
+        print("Multiple statepoint files found. Please retry")
+        sys.exit()
+    '''
+
+    args=['-s', n_threads]
+    statepoint_interval = 10
+
+    # TODO Get OpenMC instance to run in memory
+    openmc_run = init_openmc_run(prob_type, test_num, max_window_size)
+
+    with openmc_run.run_in_memory(args=args, intracomm=local_comm, seed=seed):
+        for _ in openmc_run.iter_batches():
+            curr_gen = capi.current_batch()
+            
+            if local_comm.Get_rank() == 0:
+                entropy_p = capi.entropy_p()
+                entropy = np.sum(np.log(entropy_p[entropy_p>0])/(np.log(2)) * entropy_p[entropy_p>0])*-1
+                fet_tallies = capi.convergence_tally()
+
+                if curr_gen == 1:
+                    fet_data = np.empty((0,1+len(labels)), float)
+                    entropy_data = np.empty((0,2+len(entropy_p)), float)
+
+                # Compute scaled FET coefficients
+                a_n = np.product(coeffs, axis=1) * fet_tallies
+
+                # Store a_n, curr_gen, and entropy to numpy array
+                fet_data = np.vstack((fet_data, [curr_gen] + list(a_n)))
+                entropy_data = np.vstack((entropy_data, [curr_gen] + [entropy] + list(entropy_p.flatten())))
+
+            # Create new statepoint, remove previous one and save numpy arrays
+            if curr_gen % statepoint_interval == 0:
+                #openmc_run.statepoint_write()
+                if local_comm.Get_rank() == 0:
+                    np.save("entropy_data_seed{}".format(seed), entropy_data)
+                    np.save("fet_data_seed{}".format(seed), fet_data)
+                    '''
+                    # Remove previous statepoint if more than one exists
+                    if curr_gen != statepoint_interval:
+                        os.system('rm {}'.format(prev_sp))
+                    # Update previous statepoint
+                    prev_sp = glob.glob(os.path.join("statepoint.*.h5"))[0]
+                    '''
+
+    # End of simulation, save fet and entropy data
+    if local_comm.Get_rank() == 0:
+        np.save("entropy_data_seed{}".format(seed), entropy_data)
+        np.save("fet_data_seed{}".format(seed), fet_data)
---
+++ examples/phd_testing/ensemble-averaging/timing-tests/scripts/generate_ea_examples.py
@@ -0,0 +1,210 @@
+import sys
+import socket
+import os
+
+def generate_input_files(cluster, prob_type, run_file):
+    if cluster == "Green Cluster":
+        param_dict = {
+            '1d-homog': {
+                'batch_file': "job.slurm",
+                'params_file': '1dh-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['1dh-unif-settings.xml', '1dh-geometry.xml', '1dh-materials.xml'],
+                'ppn': 32,
+                'walltime': '12:00:00',
+                'mult_factor': 3,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 32
+            },
+            '1d-homog-offset': {
+                'batch_file': "job.slurm",
+                'params_file': '1dh-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['1dh-offset-settings.xml', '1dh-geometry.xml', '1dh-materials.xml'],
+                'ppn': 32,
+                'walltime': '12:00:00',
+                'mult_factor': 3,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 32
+            },
+            '2d-beavrs': {
+                'batch_file': "job.slurm",
+                'params_file': '2db-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['2db-settings.xml', '2db-geometry.xml', '2db-materials.xml'],
+                'ppn': 32,
+                'walltime': '12:00:00',
+                'mult_factor': 3,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 8
+            }
+        }
+
+    elif cluster == "LCRC Cluster":
+        param_dict = {
+            '1d-homog': {
+                'batch_file': "job.slurm",
+                'params_file': '1dh-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['1dh-unif-settings.xml', '1dh-geometry.xml', '1dh-materials.xml'],
+                'ppn': 36,
+                'walltime': '12:00:00',
+                'mult_factor': 32,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 32
+            },
+            '1d-homog-offset': {
+                'batch_file': "job.slurm",
+                'params_file': '1dh-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['1dh-offset-settings.xml', '1dh-geometry.xml', '1dh-materials.xml'],
+                'ppn': 36,
+                'walltime': '12:00:00',
+                'mult_factor': 32,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 32
+            },
+            '2d-beavrs': {
+                'batch_file': "job.slurm",
+                'params_file': '2db-params.cfg',
+                'run_command': "sbatch ",
+                'base_files': ['2db-settings.xml', '2db-geometry.xml', '2db-materials.xml'],
+                'ppn': 36,
+                'walltime': '12:00:00',
+                'mult_factor': 32,
+                'n_particles': 10000000,
+                'n_procs_per_seed': 2,
+                'max_window_size': 8
+            }
+        }
+
+    if prob_type not in param_dict:
+        print('Problem type {} not recognized'.format(prob_type))
+        sys.exit()
+    cluster_params = param_dict[prob_type]
+
+    os.chdir('./../')
+
+    # Generate file templates
+    with open('base/{}'.format(cluster_params['batch_file']), 'r') as file:
+        batch_template = file.read()
+
+    with open('base/{}'.format(cluster_params['params_file']), 'r') as file:
+        params_template = file.read()
+
+    # Generate files for each problem type
+    os.system('mkdir -p {}'.format(prob_type))
+    os.chdir(prob_type)
+
+    run_strats = [
+        {'particle_mult': False, 'seed_mult': True, 'test_num': 0, 'ea_run': False},          # test1-32seed-10M-noCMFD
+        {'particle_mult': True, 'seed_mult': False, 'test_num': 2, 'ea_run': False},          # test2-1seed-320M-CMFD-window32
+        {'particle_mult': False, 'seed_mult': True, 'ea_run': True, 'asynchronous': 'false'}, # test3-32seed-10M-bsCMFD-window32
+        {'particle_mult': False, 'seed_mult': True, 'ea_run': True, 'asynchronous': 'true'}   # test4-32seed-10M-eaCMFD-window32
+    ]
+
+    test_num = 0
+    for run_strat in run_strats:
+        test_num += 1
+
+        run_strat['n_particles'] = cluster_params['mult_factor']*cluster_params['n_particles'] if run_strat['particle_mult'] else cluster_params['n_particles']
+        run_strat['n_seeds'] = cluster_params['mult_factor'] if run_strat['seed_mult'] else 1
+
+        if 'test_num' in run_strat:
+            run_type = 'noCMFD' if run_strat['test_num'] == 0 else 'CMFD-window{}'.format(cluster_params['max_window_size'])
+        else:
+            run_type = 'bsCMFD-window{}'.format(cluster_params['max_window_size']) if run_strat['asynchronous'] == 'false' else 'eaCMFD-window{}'.format(cluster_params['max_window_size'])
+
+        ea_run = run_strat['ea_run']
+        dir_name = 'test{}-{}seed-{}M-{}'.format(test_num, run_strat['n_seeds'], int(run_strat['n_particles']/1000000), run_type)
+        run_strat['name'] = dir_name
+        run_strat['nodes'] = cluster_params['mult_factor']+1 if ea_run else cluster_params['mult_factor']
+        os.system('mkdir -p {}'.format(dir_name))
+        os.chdir(dir_name)
+        for base_file in cluster_params['base_files']:
+            new_file = base_file.split('-')[-1] if 'xml' in base_file else base_file
+            os.system('cp ./../../base/{} ./{}'.format(base_file, new_file))
+
+        if ea_run:
+            os.system('cp ./../../base/run_openmc_ea.py ./run_openmc.py')
+        else:
+            os.system('cp ./../../base/run_openmc_noea.py ./run_openmc.py')
+             
+        create_files(batch_template, params_template, cluster_params, prob_type, run_file, run_strat)
+        os.chdir('./..')
+    os.chdir('./..')
+
+def create_files(batch_template, params_template, cluster_params, prob_name, run_file, run_strat):
+    os.system("sed -i 's-{n_particles}"+"-{}-g' settings.xml".format(run_strat['n_particles']))
+    test_num = run_strat['name'].split('-')[0]
+    jobname = test_num+'-'+prob_name
+    nodes = run_strat['nodes']
+    tasks = nodes * cluster_params['ppn']
+    procs_per_seed = cluster_params['n_procs_per_seed']
+    nprocs = nodes * procs_per_seed
+    nthreads = int(cluster_params['ppn']/procs_per_seed)
+
+    batch_template = batch_template.replace('{job_name}', jobname)
+    batch_template = batch_template.replace('{nodes}', str(nodes))
+    batch_template = batch_template.replace('{ppn}', str(cluster_params['ppn']))
+    batch_template = batch_template.replace('{walltime}', cluster_params['walltime'])
+    batch_template = batch_template.replace('{nproc}', str(nprocs))
+    if run_strat['ea_run']:
+        openmc_args = prob_name
+        params_template = params_template.replace('{n_seeds}', str(run_strat['n_seeds']))
+        params_template = params_template.replace('{n_procs_per_seed}', str(procs_per_seed))
+        params_template = params_template.replace('{asynchronous}', run_strat['asynchronous'])
+        params_template = params_template.replace('{max_window_size}', str(cluster_params['max_window_size']))
+        params_template = params_template.replace('{n_particles}', str(run_strat['n_particles']))
+        params_template = params_template.replace('{n_threads}', str(nthreads))
+        with open('params.cfg', 'w') as f:
+            f.write(params_template)
+
+    else:
+        max_window_size = cluster_params['max_window_size'] if run_strat['test_num'] == 2 else 0
+        openmc_args = "{} {} {} {} {}".format(run_strat['n_seeds'], nthreads, prob_name, run_strat['test_num'], max_window_size)
+    batch_template = batch_template.replace('{openmc_args}', openmc_args)
+    with open(cluster_params['batch_file'], 'w') as f:
+        f.write(batch_template)
+
+    print_str = os.getcwd().split('ensemble-averaging/')[-1]
+    if run_file:
+        print('Running batch script in {}'.format(print_str))
+        os.system(cluster_params['run_command']+cluster_params['batch_file'])
+    else:
+        print('Created input files in {}'.format(print_str))
+
+def get_cluster(socket_name):
+    if socket_name.startswith('nsecluster'):
+        return "NSE Cluster"
+    elif socket_name.startswith('eofe'):
+        return "Green Cluster"
+    elif socket_name.startswith('sk-linux'):
+        return "SK Linux"
+    elif socket_name.startswith('falcon'):
+        return "INL Cluster"
+    elif socket_name.startswith('bebop'):
+        return "LCRC Cluster"
+    else:
+        print('Unrecognized socket name')
+        sys.exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) not in [2, 3]:
+        print('Usage: generate_cmfd_fet_examples.py [prob_type] [-r]')
+        sys.exit()
+
+    # Get command line arguments
+    prob_type = sys.argv[1]
+    run_file = len(sys.argv) == 3 and sys.argv[2] == '-r'
+
+    # Get cluster where script is running on
+    cluster = get_cluster(socket.gethostname())
+
+    # Generate OpenMC and batch script input files
+    generate_input_files(cluster, prob_type, run_file)
